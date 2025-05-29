2cd5e87b8a5866795f956f105aa366416506a622
marceloabf <celo.filho1@hotmail.com>
2025-05-29 13:40:42 -0300

    [portfolio] Adding new page and gradually inserting texts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,17 @@ export const routes: Routes = [
     {
         path: 'home',
         loadComponent: () => import("./home/home.component").then((x) => x.HomeComponent)
+    },
+    {
+        path: 'about',
+        loadComponent: () => import("./about/about.component").then((x) => x.AboutComponent)
+    },
+    {
+        path: 'projects',
+        loadComponent: () => import("./projects/projects.component").then((x) => x.ProjectsComponent)
+    },
+    {
+        path: 'experience',
+        loadComponent: () => import("./experience/experience.component").then((x) => x.ExperienceComponent)
     }
 ];
